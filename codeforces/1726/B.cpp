ed585c12682be7ff82587bcec9dc38f3f657b189
Mohammad Sifat <sifat.sust.cse20@gmail.com>
2022-09-09 08:35:00 +0600

    Add solution for problem `B - Mainak and Interesting Sequence` Link: https://codeforces.com/contest/1726/problem/B Tags: bitmasks, constructive algorithms, math, *1100 Ref: https://codeforces.com/contest/1726/submission/171460460

---
+++ codeforces/1726/B.cpp
@@ -0,0 +1,100 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll    long long 
+#define lg     unsigned long long
+#define fi(i,L,R) for (ll i = L ; i <= R ; i++)
+#define fd(i,R,L) for (ll i = R ; i >= L ; i--)
+#define s1      string
+#define p_b     push_back
+#define st(n)   sort(a,a+n)
+#define rev     reverse(a,a+n)
+#define revs(j) reverse((j).begin(),(j).end())
+#define srt(k)   sort((k).begin(),(k).end())
+#define suni s.erase(unique(s.begin(),s.end()),s.end())
+#define vuni v.erase(unique(v.begin(),v.end()),v.end())
+//#define mode 998244353
+bool isP(ll n){
+    bool flag=true;
+    for(ll i = 2; i <= n / 2; i++) {
+       if(n % i == 0) {
+          flag = false;
+          break;
+       }
+    }
+    return flag;
+}
+
+//#define sz 1000
+//vector<ll>adj[sz],cost[sz];
+
+//ll graph[sz][sz];
+int main()
+{
+   
+//     int n, m, x, y, z;
+//     cin >> n >> m;
+//     for (int i =1; i<=m; i++)
+//     {
+//         cin >> x >> y >> z;
+//         adj[x].push_back(y);
+//         cost[x].push_back(z);
+//     }
+//     for (int i =1; i<=n; i++)
+//     {
+//         cout << i <<": ";
+//         for (int j = 0; j<adj[i].size(); j++)
+//             cout << adj[i][j]<<","<<cost[i][j]<< ",\n"[j==adj[i].size()-1] ;
+//         if(adj[i].size()==0)cout << "\n";
+//     }
+
+// /**
+// 10 9
+// 1 2 5
+// 1 3 3
+// 6 4 1
+// 6 5 5
+// 4 3 2
+// 5 3 1
+// 7 8 9
+// 8 9 3
+// 10 3 100
+// */
+      
+    ll t; cin>>t; 
+    while(t--)
+    {
+        ll i,c=0,n,m; cin>>n>>m; 
+        if(m<n||n%2==0 && m%2!=0) cout<<"NO"<<endl;
+        else
+        {
+            cout<<"YES"<<endl;
+            if(n%2!=0)
+            {
+                for(i=1;i<=n-1;i++)
+                {
+                    cout<<1<<" ";
+                }
+                cout<<m-(n-1)<<endl;
+            }
+            else if(n%2==0)
+            {
+                for(i=1;i<=n-2;i++)
+                {
+                    cout<<1<<" ";
+                }
+                for(i=1;i<=2;i++)
+                {
+                    cout<<(m+2-n)/2<<" ";
+                }
+                cout<<endl;
+            }
+            
+        }
+        
+    }
+        
+      
+
+   
+   return 0;
+}
