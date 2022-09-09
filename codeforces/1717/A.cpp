5628cb17a82edb7fa210af1e3b8368637af2bb6a
Mohammad Sifat <sifat.sust.cse20@gmail.com>
2022-09-09 08:38:00 +0600

    Add solution for problem `A - Madoka and Strange Thoughts` Link: https://codeforces.com/contest/1717/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1717/submission/171460606

---
+++ codeforces/1717/A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int t; cin>>t; 
+   while(t--)
+   {
+       int n,c=0; cin>>n; 
+       int result=n+2*(ceil(n/2)+ceil(n/3));
+       
+       cout<<result<<endl;
+       
+       
+       
+       
+       
+   }
+    
+   
+    
+    
+   return 0;
+}
