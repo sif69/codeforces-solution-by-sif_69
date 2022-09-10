59acb5bf55f32efea0bb606a66c92f3454d7fbc3
Mohammad sifat <sifat.sust.cse20@gmail.com>
2022-09-11 01:55:00 +0600

    Add solution for problem `A - XOR Mixup` Link: https://codeforces.com/contest/1698/problem/A Tags: bitmasks, brute force, *800 Ref: https://codeforces.com/contest/1698/submission/171653267

---
+++ codeforces/1698/A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll   unsigned long long
+#define lg   long long
+#define f    for(i=0;i<n;i++)
+#define f1   for(j=0;j<m;j++)
+#define f2   for(i=0;i<s.size();i++)
+#define f3   for(i=n;i>=0;i--)
+#define f4   for(i=s.size();i>=0;i--)
+#define s1   string
+#define p_b  push_back
+#define st   sort(a,a+n)
+#define sts  sort(s.begin(),s.end())
+#define stv  sort(v.begin(),v.end())
+#define suni s.erase(unique(s.begin(),s.end()),s.end())
+#define vuni v.erase(unique(v.begin(),v.end()),v.end())
+
+
+
+int main()
+{
+
+  ll t ;cin>>t; while(t--)
+  {
+       ll n,i;cin>>n;
+       ll a [n]; f cin>>a[i];
+       cout<<a[0]<<endl;
+
+  }
+
+   return 0;
+}
