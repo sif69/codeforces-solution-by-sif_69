29244c6a6c8dc8ca58e4f678bb5cf45cbd64b8f9
Mohammad Sifat <sifat.sust.cse20@gmail.com>
2023-04-27 21:10:00 +0600

    Add solution for problem `A - A-characteristic` Link: https://codeforces.com/contest/1823/problem/A Tags: combinatorics, constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1823/submission/203691009

---
+++ codeforces/1823/A.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll      long long
+#define lg     long long
+#define fi(i,L,R) for (ll i = L ; i <= R ; i++)
+#define fd(i,R,L) for (ll i = R ; i >= L ; i--)
+#define s1      string
+#define p_b     push_back
+#define st(n)   sort(a,a+n)
+#define rev     reverse(a,a+n)
+#define revs(j) reverse((j).begin(),(j).end())
+#define srt(k)   sort((k).begin(),(k).end())
+#define suni s.erase(unique(s.begin(),s.end()),s.end())
+#define vuni v.erase(unique( v.begin(),v.end()),v.end())
+#define puni v1.erase(unique(v1.begin(),v1.end()),v1.end())
+#define  yo    cout<<"YES"<<endl
+#define  no    cout<<"NO"<<endl
+#define M 1000000007
+
+bool isPrime(int num){
+    bool flag=true;
+    for(int i = 2; i <= num / 2; i++) {
+       if(num % i == 0) {
+          flag = false;
+          break;
+       }
+    }
+    if(num==1) return flag=false;
+
+    return flag;
+}
+
+
+
+
+void Solve()
+{
+             ll i,j,c=0,flag=0,n; 
+           ll k; cin>>n>>k; 
+          for(i=1;i<=n;i++)
+          {
+            ll l,r;
+            l=i;
+            r=n-i;
+            if((l*(l-1))/2+(r*(r-1))/2==k)
+            {
+                flag=l;
+                break;
+            }
+          }
+          if(flag==0)
+          {
+            no;
+          }
+          else
+          {
+            yo;
+            for(i=0;i<flag;i++)
+            {
+                cout<<1<<" ";
+            }
+            for(i=0;i<abs(flag-n);i++)
+            {
+                cout<<-1<<" ";
+            }
+            cout<<endl;
+
+          }
+            
+           
+          
+
+ 
+          
+
+            
+        
+          
+       
+        
+          
+           
+          
+            
+
+
+   
+      
+           
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int tt=1;
+        
+
+        cin>>tt;
+
+        while(tt--)
+        {
+
+            Solve();
+        }
+
+        return 0;
+}
