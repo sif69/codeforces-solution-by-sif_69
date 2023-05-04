eeafd7aa40d65c0f24b9ddd5995f92a0bed2afff
Mohammad Sifat <sifat.sust.cse20@gmail.com>
2023-05-04 22:49:00 +0600

    Add solution for problem `B - Special Permutation` Link: https://codeforces.com/contest/1612/problem/B Tags: constructive algorithms, greedy, *900 Ref: https://codeforces.com/contest/1612/submission/204482134

---
+++ codeforces/1612/B.cpp
@@ -0,0 +1,119 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll      long long
+#define lg     long long
+#define fi(i,L,R) for (ll i = L ; i <= R ; i++)
+#define fd(i,R,L) for (ll i = R ; i >= L ; i--)
+#define s1      string
+#define p_b     push_back
+#define st(n)   sort(a,a+n)
+#define rev     reverse(a,a+n)
+#define revs(j) reverse((j).begin(),(j).end())
+#define srt(k)   sort((k).begin(),(k).end())
+#define suni s.erase(unique(s.begin(),s.end()),s.end())
+#define vuni v.erase(unique( v.begin(),v.end()),v.end())
+#define puni v1.erase(unique(v1.begin(),v1.end()),v1.end())
+#define  yo    cout<<"YES"<<endl
+#define  no    cout<<"NO"<<endl
+#define M 1000000007
+
+bool isPrime(int num){
+    bool flag=true;
+    for(int i = 2; i <= num / 2; i++) {
+       if(num % i == 0) {
+          flag = false;
+          break;
+       }
+    }
+    if(num==1) return flag=false;
+
+    return flag;
+}
+
+
+ll dig(ll n)
+{
+    ll c=0;
+    while(n)
+    {
+        n/=10;
+        c++;
+    }
+    return c;
+}
+// ll vis[1]
+void Solve()
+{
+            ll i,c=0,flag=0,j,n;
+            ll a,b; cin>>n>>a>>b;
+            vector<ll>v;
+            if(n<=2)
+            {
+                cout<<a<<" "<<b<<endl;
+                return;
+            }
+           
+            
+            else
+            {
+                for(i=n;i>n/2;i--)
+                {
+                    if(i==b)
+                     v.p_b(a); 
+                
+                 else v.p_b(i); 
+                }
+                for(i=n/2;i>=1;i--)
+                {
+                    if(i==a)
+                        v.p_b(b);
+                    else v.p_b(i); 
+                }
+               ll mx=-1,mn=1e12;
+               for(i=0;i<v.size()/2;i++)
+               {
+                  mn=min(mn,v[i]);
+               }
+               for(i=v.size()/2;i<v.size();i++)
+               {
+                mx=max(mx,v[i]);
+               }
+               if(mn==a&&mx==b)
+               {
+                for(i=0;i<v.size();i++)
+                {
+                    cout<<v[i]<<" ";
+                }
+                cout<<endl;
+               }
+               else
+               {
+                cout<<-1<<endl;
+               }
+            }
+
+          
+        
+         
+                                
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int tt=1;
+        
+
+        cin>>tt;
+
+        while(tt--)
+        {
+
+            Solve();
+        }
+
+        return 0;
+}
