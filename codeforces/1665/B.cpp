25211f044aba8af39fc1405268108bd01873beae
Mohammad Sifat <sifat.sust.cse20@gmail.com>
2023-05-05 19:35:00 +0600

    Add solution for problem `B - Array Cloning Technique` Link: https://codeforces.com/contest/1665/problem/B Tags: constructive algorithms, greedy, sortings, *900 Ref: https://codeforces.com/contest/1665/submission/204565592

---
+++ codeforces/1665/B.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll      long long
+#define lg     long long
+#define fi(i,L,R) for (ll i = L ; i <= R ; i++)
+#define fd(i,R,L) for (ll i = R ; i >= L ; i--)
+#define s1      string
+#define p_b     push_back
+#define st(n)   sort(a,a+n)
+#define rev     reverse(a,a+n)
+#define revs(j) reverse((j).begin(),(j).end())
+#define srt(k)   sort((k).begin(),(k).end())
+#define suni s.erase(unique(s.begin(),s.end()),s.end())
+#define vuni v.erase(unique( v.begin(),v.end()),v.end())
+#define puni v1.erase(unique(v1.begin(),v1.end()),v1.end())
+#define  yo    cout<<"YES"<<endl
+#define  no    cout<<"NO"<<endl
+#define M 1000000007
+
+bool isPrime(int num){
+    bool flag=true;
+    for(int i = 2; i <= num / 2; i++) {
+       if(num % i == 0) {
+          flag = false;
+          break;
+       }
+    }
+    if(num==1) return flag=false;
+
+    return flag;
+}
+
+
+ll dig(ll n)
+{
+    ll c=0;
+    while(n)
+    {
+        n/=10;
+        c++;
+    }
+    return c;
+}
+// ll vis[1]
+void Solve()
+{
+            ll i,c=0,flag=0,j,n;
+            cin>>n; 
+            ll a[n]; 
+            map<ll,ll>mp;
+            ll mx=0;
+            for(i=0;i<n;i++)
+
+            {
+                cin>>a[i]; 
+                mp[a[i]]++; 
+                mx=max(mx,mp[a[i]]);
+            }
+            ll sum=0;
+            n-=mx;
+           while(n)
+           {
+            sum+=(min(mx,n));
+              n-=min(mx,n);
+             c++;
+             
+             mx*=2;
+             
+           }
+           sum+=c;
+           cout<<sum<<endl;
+
+
+           
+           
+            
+           
+              
+
+
+          
+        
+         
+                                
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int tt=1;
+        
+
+        cin>>tt;
+
+        while(tt--)
+        {
+
+            Solve();
+        }
+
+        return 0;
+}
