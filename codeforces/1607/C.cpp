824baa645d8a22971eae6bf1e125e196e180700b
Mohammad Sifat <sifat.sust.cse20@gmail.com>
2023-05-15 13:33:00 +0600

    Add solution for problem `C - Minimum Extraction` Link: https://codeforces.com/contest/1607/problem/C Tags: brute force, sortings, *1000 Ref: https://codeforces.com/contest/1607/submission/205952758

---
+++ codeforces/1607/C.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll      long long
+#define lg     long long
+#define fi(i,L,R) for (ll i = L ; i <= R ; i++)
+#define fd(i,R,L) for (ll i = R ; i >= L ; i--)
+#define s1      string
+#define p_b     push_back
+#define st(n)   sort(a,a+n)
+#define rev     reverse(a,a+n)
+#define revs(j) reverse((j).begin(),(j).end())
+#define srt(k)   sort((k).begin(),(k).end())
+#define suni s.erase(unique(s.begin(),s.end()),s.end())
+#define vuni v.erase(unique( v.begin(),v.end()),v.end())
+#define puni v1.erase(unique(v1.begin(),v1.end()),v1.end())
+#define  yo    cout<<"YES"<<endl
+#define  no    cout<<"NO"<<endl
+#define M 1000000007
+#define pp endl
+
+bool isPrime(int num){
+    bool flag=true;
+    for(int i = 2; i <= num / 2; i++) {
+       if(num % i == 0) {
+          flag = false;
+          break;
+       }
+    }
+    if(num==1) return flag=false;
+
+    return flag;
+}
+
+
+ 
+void Solve()
+{
+            ll i,c=0,flag=0,j,n,sum=0,cnt=0;
+            cin>>n; 
+            vector<ll>v(n);
+            fi(i,0,n-1)
+            {
+                cin>>v[i];
+            }
+            srt(v); 
+            vuni ;
+         
+          
+            ll mn=0;
+             mn=0;
+            sum+=0;
+          for(i=0;i<v.size();i++)
+          {
+             if(i==0)
+             {
+                mn=v[0]; 
+                sum=v[0]; 
+                continue;
+             }
+                mn=max(mn,abs(v[i]-sum));
+                sum+=(v[i]-sum);
+
+          }
+          // ll p=
+         if(v.size()==1&&n>1)
+         {
+              mn=max(v[0]-v[0],v[0]);
+         }
+         cout<<mn<<pp;
+           
+           
+         
+        
+                   
+
+           
+         
+         
+
+
+
+           
+
+  // transform(s.begin(), s.end(), s.begin(), ::tolower);
+           
+
+           
+              
+
+
+          
+        
+         
+                                
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int tt=1;
+        // jo();
+        
+
+        cin>>tt;
+
+        while(tt--)
+        {
+
+            Solve();
+        }
+
+        return 0;
+}
