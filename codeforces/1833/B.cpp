658a40732d7b682e187a7db8e58b436684122976
Mohammad Sifat <sifat.sust.cse20@gmail.com>
2023-05-19 21:12:00 +0600

    Add solution for problem `B - Restore the Weather` Link: https://codeforces.com/contest/1833/problem/B Tags: greedy, sortings, *900 Ref: https://codeforces.com/contest/1833/submission/206492622

---
+++ codeforces/1833/B.cpp
@@ -0,0 +1,109 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll      long long
+#define lg      long long
+#define fi(i,L,R) for (ll i = L ; i <= R ; i++)
+#define fd(i,R,L) for (ll i = R ; i >= L ; i--)
+#define s1      string
+#define p_b     push_back
+#define st(n)   sort(a,a+n)
+#define rev     reverse(a,a+n)
+#define revs(j) reverse((j).begin(),(j).end())
+#define srt(k)  sort((k).begin(),(k).end())
+#define suni    s.erase(unique(s.begin(),s.end()),s.end())
+#define vuni    v.erase(unique( v.begin(),v.end()),v.end())
+#define puni    v1.erase(unique(v1.begin(),v1.end()),v1.end())
+#define  yo     cout<<"YES"<<endl
+#define  no     cout<<"NO"<<endl
+#define  M      1000000007
+#define pie acos(-1.0)
+#define pp endl
+bool isPrime(int num){
+    bool flag=true;
+    for(int i = 2; i <= num / 2; i++) {
+       if(num % i == 0) {
+          flag = false;
+          break;
+       }
+    }
+    if(num==1) return flag=false;
+
+    return flag;
+}
+
+
+  
+
+void Solve()
+{
+
+         ll i,j,c=0,cnt=0,flag=0,n;
+         
+       
+        ll k; 
+         cin>>n>>k;
+         ll a[n],b[n],ans[n];
+         vector<pair<ll,ll>>v;
+         vector<ll>v1;
+         fi(i,0,n-1) {
+            cin>>a[i];
+            v.p_b({a[i],i});
+         } 
+         fi(i,0,n-1){
+            cin>>b[i];
+         }
+          // auto itr = find_if(a, a + n, comp(elem));
+ 
+         // sort(a,a+n); 
+         sort(b,b+n);
+         srt(v);
+         for(auto x:v){
+              v1.p_b(x.second);
+         }
+         for(i=0;i<n;i++){
+            ans[v1[i]]=b[i];
+         }
+         for(i=0;i<n;i++){
+            cout<<ans[i]<<" ";
+         }
+         cout<<pp;
+        
+        
+
+
+         
+         
+
+
+         
+         
+         
+         
+
+          
+         
+          
+
+      
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int tt=1;
+        
+
+        cin>>tt;
+
+        while(tt--)
+        {
+
+            Solve();
+        }
+
+        return 0;
+}
