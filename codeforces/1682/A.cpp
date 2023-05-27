a71b8585172aaa884a168caa71af5e1a74e77e20
Mohammad Sifat <sifat.sust.cse20@gmail.com>
2023-05-27 20:13:00 +0600

    Add solution for problem `A - Palindromic Indices` Link: https://codeforces.com/contest/1682/problem/A Tags: greedy, strings, *800 Ref: https://codeforces.com/contest/1682/submission/207477945

---
+++ codeforces/1682/A.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll       long long
+#define lg       long long
+#define fi(i,L,R) for (ll i = L ; i <= R ; i++)
+#define fd(i,R,L) for (ll i = R ; i >= L ; i--)
+#define s1      string
+#define p_b     push_back
+#define st(n)   sort(a,a+n)
+#define rev     reverse(a,a+n)
+#define revs(j) reverse((j).begin(),(j).end())
+#define srt(k)  sort((k).begin(),(k).end())
+#define suni    s.erase(unique(s.begin(),s.end()),s.end())
+#define vuni    v.erase(unique( v.begin(),v.end()),v.end())
+#define puni    v1.erase(unique(v1.begin(),v1.end()),v1.end())
+#define  yo     cout<<"YES"<<endl
+#define  no     cout<<"NO"<<endl
+#define  M      1000000007
+#define pie acos(-1.0)
+#define pp endl
+#define gg greater<ll>()
+
+
+
+
+void Solve()
+{
+
+   
+
+     ll i,c=0,cnt=0,flag=0,j,n,sum=0;
+     cin>>n; 
+     s1 s; cin>>s; 
+     ll p=n/2;
+     char track=s[p];
+     cnt++;
+     for(i=p+1;i<n;i++){
+        if(s[i]==track) cnt++;
+        else break;
+     }
+     for(i=p-1;i>=0;i--){
+        if(s[i]==track) cnt++;
+        else break;
+     }
+     cout<<cnt<<pp;
+    
+     
+    
+     
+    
+  
+   
+        
+     
+        
+    
+    
+   
+
+     
+      
+     
+    
+    
+   
+   
+   
+    
+
+    
+     
+    
+   
+    
+
+
+     
+    
+
+
+       
+          
+
+
+         
+            
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+        cin.tie(0);
+       
+        int tt=1;
+        
+
+        cin>>tt;
+
+        while(tt--)
+        {
+
+            Solve();
+        }
+
+        return 0;
+}
