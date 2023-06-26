c8bc70bac6d76a1415ccec453598acdfc9793414
Mohammad Sifat <sifat.sust.cse20@gmail.com>
2023-06-26 08:05:00 +0600

    Add solution for problem `A - Tenzing and Tsondu` Link: https://codeforces.com/contest/1842/problem/A Tags: games, math, *800 Ref: https://codeforces.com/contest/1842/submission/211086791

---
+++ codeforces/1842/A.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll      long long
+#define lg     long long
+#define fi(i,L,R) for (ll i = L ; i <= R ; i++)
+#define fd(i,R,L) for (ll i = R ; i >= L ; i--)
+#define s1      string
+#define p_b     push_back
+#define st(n)   sort(a,a+n)
+#define rev     reverse(a,a+n)
+#define revs(j) reverse((j).begin(),(j).end())
+#define srt(k)   sort((k).begin(),(k).end())
+#define suni s.erase(unique(s.begin(),s.end()),s.end())
+#define vuni v.erase(unique( v.begin(),v.end()),v.end())
+#define puni v1.erase(unique(v1.begin(),v1.end()),v1.end())
+#define  yo    cout<<"YES"<<endl
+#define  no    cout<<"NO"<<endl
+#define pp endl
+#define M 1000000007
+
+bool isPrime(int num){
+    bool flag=true;
+    for(int i = 2; i <= num / 2; i++) {
+       if(num % i == 0) {
+          flag = false;
+          break;
+       }
+    }
+    if(num==1) return flag=false;
+
+    return flag;
+}
+
+
+ll d1[]={-1,-1,-1,0,1,1,1,0};
+ll d2[]={0,-1,1,-1,0,1,-1,1};
+
+
+
+void Solve()
+{
+
+               
+             ll i,j,c=0,cnt=0,flag=0,n,sum=0;
+             ll m; 
+             cin>>n>>m; 
+             ll a[n],b[m];ll sum1=0;
+             fi(i,0,n-1){
+                cin>>a[i];
+                sum+=a[i];
+             }
+             fi(i,0,m-1){
+                cin>>b[i]; 
+                sum1+=b[i];
+
+             }
+             if(sum>sum1) cout<<"Tsondu"<<pp;
+             else if(sum<sum1) cout<<"Tenzing"<<pp;
+             else cout<<"Draw"<<pp;
+                 
+ 
+   
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int tt=1;
+        
+
+        cin>>tt;
+
+        while(tt--)
+        {
+
+            Solve();
+        }
+
+        return 0;
+}
