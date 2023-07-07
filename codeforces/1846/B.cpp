8d1d3b0d5f804e11da805a5399077f276ac9ccc4
Mohammad Sifat <sifat.sust.cse20@gmail.com>
2023-07-07 20:50:00 +0600

    Add solution for problem `B - Rudolph and Tic-Tac-Toe` Link: https://codeforces.com/contest/1846/problem/B Tags: brute force, implementation, strings, *800 Ref: https://codeforces.com/contest/1846/submission/212586916

---
+++ codeforces/1846/B.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define  ll        long long
+#define  lg        long long
+#define  fi(i,L,R) for (ll i = L ; i <= R ; i++)
+#define  fd(i,R,L) for (ll i = R ; i >= L ; i--)
+#define  s1        string
+#define  p_b       push_back
+#define  st(n)     sort(a,a+n)
+#define  rev       reverse(a,a+n)
+#define  revs(j)   reverse((j).begin(),(j).end())
+#define  srt(k)    sort((k).begin(),(k).end())
+#define  suni      s.erase(unique(s.begin(),s.end()),s.end())
+#define  vuni      v.erase(unique( v.begin(),v.end()),v.end())
+#define  puni      v1.erase(unique(v1.begin(),v1.end()),v1.end())
+#define  yo       cout<<"YES"<<endl
+#define  no       cout<<"NO"<<endl
+#define  M        1000000007
+#define  pie       acos(-1.0)
+#define  pp        endl 
+
+
+bool isPrime(int num){
+    bool flag=true;
+    for(int i = 2; i <= num / 2; i++) {
+       if(num % i == 0) {
+          flag = false;
+          break;
+       }
+    }
+    if(num==1) return flag=false;
+
+    return flag;
+}
+
+
+
+
+void Solve()
+{
+
+          ll i,j,cnt=0,flag=0,c=0;
+          s1 s,p,l; cin>>s>>p>>l;
+          if(s[0]==s[1]&&s[1]==s[2]&&s[0]!='.'){
+            cout<<s[0]<<pp;
+
+          }
+          else if(p[0]==p[1]&&p[1]==p[2]&&p[0]!='.'){
+            cout<<p[0]<<pp;
+          }
+          else if(l[0]==l[1]&&l[1]==l[2]&&l[0]!='.'){
+            cout<<l[0]<<pp;
+          }
+          else if(s[0]==p[0]&&p[0]==l[0]&&s[0]!='.'){
+            cout<<s[0]<<pp;
+          }
+          else if(s[1]==p[1]&&p[1]==l[1]&&s[1]!='.'){
+            cout<<s[1]<<pp;
+          }
+          else if(s[2]==p[2]&&p[2]==l[2]&&s[2]!='.'){
+            cout<<s[2]<<pp;
+          }
+          else if(s[0]==p[1]&&p[1]==l[2]&&s[0]!='.'){
+            cout<<s[0]<<pp;
+          }
+          else if(s[2]==p[1]&&p[1]==l[0]&&s[2]!='.'){
+            cout<<s[2]<<pp;
+          }
+          else{
+            cout<<"DRAW"<<pp;
+          }
+
+
+
+
+}
+
+
+int main()
+{ 
+
+    ios::sync_with_stdio(false);
+        cin.tie(0);
+
+
+        int tt=1;
+        
+
+        cin>>tt;
+
+        while(tt--)
+        {
+ 
+             Solve();
+
+        }
+
+
+        return 0;
+}
+              
