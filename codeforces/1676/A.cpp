77f91257d57b79bb9dd8c49233b9959f52fedb76
Mohammad Sifat <sifat.sust.cse20@gmail.com>
2023-09-02 18:59:00 +0600

    Add solution for problem `A - Lucky?` Link: https://codeforces.com/contest/1676/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1676/submission/221568518

---
+++ codeforces/1676/A.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define  ll       long long
+#define  lg        long long
+#define  fi(i,L,R) for (ll i = L ; i <= R ; i++)
+#define  fd(i,R,L) for (ll i = R ; i >= L ; i--)
+#define  s1        string
+#define  p_b       push_back
+#define  st(n)     sort(a,a+n)
+#define  rev       reverse(a,a+n)
+#define  revs(j)   reverse((j).begin(),(j).end())
+#define  srt(k)    sort((k).begin(),(k).end())
+#define  suni      s.erase(unique(s.begin(),s.end()),s.end())
+#define  vuni      v.erase(unique( v.begin(),v.end()),v.end())
+#define  puni      v1.erase(unique(v1.begin(),v1.end()),v1.end())
+#define  yo       cout<<"YES"<<endl
+#define  no       cout<<"NO"<<endl
+#define  M        1000000007
+#define  pie       acos(-1.0)
+#define  pp        endl 
+#define  sz        200000
+
+void Solve()
+  {
+              
+                ll  i , j , c=0, flag=0, sum=0, n;
+
+                s1 s; cin >> s;
+
+                ll S1,S2;
+                S1=s[0]-'0' + s[1] -'0' + s[2] - '0' ;
+                S2=s[3]-'0' + s[4] -'0' + s[5] - '0' ;
+
+                if(S1==S2) yo;
+                else no;
+          
+
+ }
+
+int main()
+{ 
+
+
+    ios::sync_with_stdio(false);
+
+        cin.tie(0);
+
+
+        int tt=1;
+
+      
+        
+
+        cin>>tt;
+
+        while(tt--)
+        {
+ 
+             Solve();
+
+        }
+
+
+        return 0;
+}
+              
