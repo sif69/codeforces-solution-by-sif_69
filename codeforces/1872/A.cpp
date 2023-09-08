7ba8cf832094d1c279643dd6fbf9c3eefe881b55
Mohammad Sifat <sifat.sust.cse20@gmail.com>
2023-09-08 11:10:00 +0600

    Add solution for problem `A - Two Vessels` Link: https://codeforces.com/contest/1872/problem/A Tags: brute force, greedy, math, *800 Ref: https://codeforces.com/contest/1872/submission/222397136

---
+++ codeforces/1872/A.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define  ll       long long
+#define  lg        long long
+#define  fi(i,L,R) for (ll i = L ; i <= R ; i++)
+#define  fd(i,R,L) for (ll i = R ; i >= L ; i--)
+#define  s1        string
+#define  p_b       push_back
+#define  st(n)     sort(a,a+n)
+#define  rev       reverse(a,a+n)
+#define  revs(j)   reverse((j).begin(),(j).end())
+#define  srt(k)    sort((k).begin(),(k).end())
+#define  suni      s.erase(unique(s.begin(),s.end()),s.end())
+#define  vuni      v.erase(unique( v.begin(),v.end()),v.end())
+#define  puni      v1.erase(unique(v1.begin(),v1.end()),v1.end())
+#define  yo       cout<<"YES"<<endl
+#define  no       cout<<"NO"<<endl
+#define  M        1000000007
+#define  pie       acos(-1.0)
+#define  pp        endl 
+#define  sz        200000
+
+void Solve()
+  {
+              
+                ll  i , j , flag=0, sum=0, n;
+
+              ll a,b,c,mn; cin >> a >> b >> c;
+              
+              if(a==b) cout << 0 << pp;
+              else{
+                 sum=abs(a-b);
+
+                 cout << (sum+2*c-1)/(2*c) << pp;
+              }
+
+ }
+
+int main()
+{ 
+
+
+    ios::sync_with_stdio(false);
+
+        cin.tie(0);
+
+
+        int tt=1;
+
+      
+        
+
+        cin>>tt;
+
+        while(tt--)
+        {
+ 
+             Solve();
+
+        }
+
+
+        return 0;
+}
+              
