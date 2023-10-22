6ad8ec02230ad43b29b6dfa139e1046eb3b09f63
Mohammad Sifat <sifat.sust.cse20@gmail.com>
2023-10-22 08:13:00 +0600

    Add solution for problem `E - Cardboard for Pictures` Link: https://codeforces.com/contest/1850/problem/E Tags: binary search, geometry, implementation, math, *1100 Ref: https://codeforces.com/contest/1850/submission/229128975

---
+++ codeforces/1850/E.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define  ll       long long
+#define  lg        long long
+#define  fi(i,L,R) for (ll i = L ; i <= R ; i++)
+#define  fd(i,R,L) for (ll i = R ; i >= L ; i--)
+#define  s1        string
+#define  p_b       push_back
+#define  st(n)     sort(a,a+n)
+#define  rev       reverse(a,a+n)
+#define  revs(j)   reverse((j).begin(),(j).end())
+#define  srt(k)    sort((k).begin(),(k).end())
+#define  suni      s.erase(unique(s.begin(),s.end()),s.end())
+#define  vuni      v.erase(unique( v.begin(),v.end()),v.end())
+#define  puni      v1.erase(unique(v1.begin(),v1.end()),v1.end())
+#define  yo       cout<<"YES"<<endl
+#define  no       cout<<"NO"<<endl
+#define  M        1000000007
+#define  pie       acos(-1.0)
+#define  pp        endl 
+#define  sz        200000
+
+typedef pair<int, int> pi;
+
+
+void Solve()
+{
+              
+        
+            ll i,j,flag=0,c=0,n,l1,R1,cnt=0;
+
+           cin >> n >> c;
+           ll a[n];
+          
+           fi(i,0,n-1){
+                 cin >> a[i];
+
+           }
+
+           ll lf=1,rt=1e9;
+
+            while(lf <= rt) {
+      ll mid = lf + (rt - lf) / 2;
+      ll sumall = 0;
+        fi(i,0,n-1) {
+
+            sumall += (a[i] + 2 * mid) * (a[i] + 2 * mid);
+            if(sumall > c) break;
+
+        }
+        if(sumall == c) {
+
+            cout << mid << "\n";
+           break;
+
+        }
+        if(sumall > c) {
+
+            rt = mid - 1;
+
+        } else {
+
+            lf = mid + 1;
+        }
+    }
+
+
+ 
+}
+
+int main()
+{ 
+
+
+    ios::sync_with_stdio(false);
+
+        cin.tie(0);
+
+
+        int tt=1;
+
+      
+        
+
+        cin>>tt;
+
+        while(tt--)
+        {
+ 
+             Solve();
+
+        }
+
+
+        return 0;
+}
+              
